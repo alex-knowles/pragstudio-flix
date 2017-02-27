8a3852d3e0147935335d9190c7f5987633af9d68
Alex Knowles <alexk@datalogics.com>
2017-02-27 13:24:05 -0600

    Define spec for listing movies.

--- spec/features/list_movies_spec.rb
+++ spec/features/list_movies_spec.rb
@@ -2,6 +2,9 @@ require 'rails_helper'
 
 describe "Viewing the list of movies" do
 
-  it "shows the movies"
+  it "shows the movies" do
+    visit 'http://example.com/movies'
+    expect(page).to have_text("3 Movies")
+  end
 
 end
