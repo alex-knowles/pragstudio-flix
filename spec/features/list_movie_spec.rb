f84a4ae571e857c10fd129f006a2774fd8c8af17
Alex Knowles <alexk@datalogics.com>
2017-02-27 21:57:43 -0600

    Define pending examples for displaying movie gross.

--- spec/features/list_movie_spec.rb
+++ spec/features/list_movie_spec.rb
@@ -9,9 +9,12 @@ describe "Viewing a movie" do
 
     expect(page).to have_text(movie.title)
     expect(page).to have_text(movie.rating)
-    expect(page).to have_text("$318,412,101.00")
     expect(page).to have_text(movie.description)
     expect(page).to have_text(movie.released_on)
   end
+
+  it "shows the total gross if the total gross exceeds $50M"
+
+  it "shows 'Flop!' if the total gross is less than $50M"
 
 end
