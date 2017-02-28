797e4fa53df584dcb992ef9377009790203e4731
Alex Knowles <alexk@datalogics.com>
2017-02-27 22:11:31 -0600

    Define movie model example for a non-flop.
    Total gross exceeds $50 M.

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -7,4 +7,9 @@ describe "A movie" do
     expect(movie.flop?).to eq(true)
   end
 
+  it "is not a flop if the total gross is more than $50M" do
+    movie = Movie.new(total_gross: 51000000.00)
+    expect(movie.flop?).to eq(false)
+  end
+
 end
