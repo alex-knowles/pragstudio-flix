c079de6200fe584395e301046c7a5d31a8683896
Alex Knowles <alexk@datalogics.com>
2017-02-28 14:56:31 -0600

    Update movie detail form to accept new values.
    Also update seed records with new values.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -38,7 +38,7 @@ class MoviesController < ApplicationController
 
     def movie_params
       params.require(:movie).permit(:title, :description, :rating, :released_on,
-       :total_gross)
+       :total_gross, :cast, :director, :duration, :image_file_name)
     end
 
 end
