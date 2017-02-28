016305ecdbe5457505ec7aae789593352d0de47e
Alex Knowles <alexk@datalogics.com>
2017-02-27 22:01:30 -0600

    Implement example for non-flop movie gross.
    Converts amount to currency.

--- spec/features/list_movie_spec.rb
+++ spec/features/list_movie_spec.rb
@@ -13,7 +13,11 @@ describe "Viewing a movie" do
     expect(page).to have_text(movie.released_on)
   end
 
-  it "shows the total gross if the total gross exceeds $50M"
+  it "shows the total gross if the total gross exceeds $50M" do
+    movie = Movie.create(movie_attributes(total_gross: 60000000.00))
+    visit movie_url(movie)
+    expect(page).to have_text("$60,000,000.00")
+  end
 
   it "shows 'Flop!' if the total gross is less than $50M"
 
