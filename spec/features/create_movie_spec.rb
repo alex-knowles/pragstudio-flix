7f78dc33467a91c295fbdf5e5ccbfe479573d824
Alex Knowles <alexk@datalogics.com>
2017-02-28 10:10:12 -0600

    Factor out a local variable in create movie spec.

--- spec/features/create_movie_spec.rb
+++ spec/features/create_movie_spec.rb
@@ -7,14 +7,15 @@ describe "Creating a new movie" do
     click_link "Add New Movie"
     expect(current_path).to eq(new_movie_path)
 
-    fill_in "Title", with: "New Movie Title"
+    expected_title = "New Movie Title"
+    fill_in "Title", with: expected_title
     fill_in "Description", with: "Superheroes saving the world from villains"
     fill_in "Rating", with: "PG-13"
     fill_in "Total gross", with: "75000000"
     fill_in "Released on", with:(Time.new.year - 1).to_s
     click_button "Create Movie"
     expect(current_path).to eq(movie_path(Movie.last))
-    expect(page).to have_text("New Movie Title")
+    expect(page).to have_text(expected_title)
   end
 
 end
