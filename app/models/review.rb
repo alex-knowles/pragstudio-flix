c49f9fb086880ac26b84fb4e1ad7ef5e8a9f0ed4
Alex Knowles <alexk@datalogics.com>
2017-03-03 21:04:17 -0600

    Reviews can have ‘stars’ values between 1 and 5.

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,5 +3,6 @@ class Review < ApplicationRecord
 
   validates :name, presence: true
   validates :comment, length: { minimum: 4 }
+  validates :stars, inclusion: { in: (1..5).to_a, message: "must be between 1 and 5" }
 
 end
