484f9237fe43c87ff3f9832acb97546b6f6dc395
Alex Knowles <alexk@datalogics.com>
2017-03-05 22:21:37 -0600

    List most recent reviews first.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,7 +3,7 @@ class ReviewsController < ApplicationController
   def index
     movie_id =  params[:movie_id]
     @movie = Movie.find(movie_id)
-    @reviews = Review.where(movie_id:movie_id)
+    @reviews = Review.where(movie_id:movie_id).order("created_at desc")
   end
 
 end
