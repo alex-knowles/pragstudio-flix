9927c4363387f68135aea93489b797b90f691bd2
Alex Knowles <alexk@datalogics.com>
2017-03-05 21:59:52 -0600

    Define ‘index’ route for reviews.
    * nest reviews routes inside movies routes
    * define “index” action for reviews controller
    * define a basic view template for review
      index

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,9 @@
 class ReviewsController < ApplicationController
+
+  def index
+    movie_id =  params[:movie_id]
+    @movie = Movie.find(movie_id)
+    @reviews = Review.where(movie_id:movie_id)
+  end
+
 end
