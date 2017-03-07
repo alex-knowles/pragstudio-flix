4399265f1409c4034a726924ae125f9e93a927ac
Alex Knowles <alexk@datalogics.com>
2017-03-06 21:37:35 -0600

    Fix indentation

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -40,11 +40,11 @@ class MoviesController < ApplicationController
     redirect_to movies_path, alert: "Movie deleted successfully!"
   end
 
-  private
+private
 
-    def movie_params
-      params.require(:movie).permit(:title, :description, :rating, :released_on,
-       :total_gross, :cast, :director, :duration, :image_file_name)
-    end
+  def movie_params
+    params.require(:movie).permit(:title, :description, :rating, :released_on,
+     :total_gross, :cast, :director, :duration, :image_file_name)
+  end
 
 end
