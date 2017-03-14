7d4d862f2ee596966fbd9ed9add2ed0c21dd1f94
Alex Knowles <alexk@datalogics.com>
2017-03-14 08:55:01 -0500

    Outline a spec for a User.
    Start off by requiring a few attributes:
    
    * name
    * email
    * password

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe 'A user' do
+
+  it 'requires a name'
+
+  it 'requires an email'
+
+  it 'accepts an email that is formatted correctly'
+
+  it 'rejects an email that is formatted incorrectly'
+
+  it 'requires a password'
+
+end
