f9b4da90795eec81dd29f42a165b75ee8ee01d53
Alex Knowles <alexk@datalogics.com>
2017-03-14 17:32:02 -0500

    Email must be formatted correctly.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :name, :email, presence: true
+  validates :name, presence: true
+  validates :email, presence: true, format: /\A\S+@\S+\z/
 end
