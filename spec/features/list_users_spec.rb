e2e1527799fe8d99d64173be8f2b3e0c20db309a
Alex Knowles <alexk@datalogics.com>
2017-03-15 17:35:52 -0500

    Test that multiple users are listed.

--- spec/features/list_users_spec.rb
+++ spec/features/list_users_spec.rb
@@ -3,9 +3,13 @@ require 'rails_helper'
 describe "Viewing the list of users" do
 
   it "shows the users" do
-    user1 = User.create(user_attributes(name: "Alice Smith"))
+    user1 = User.create!(user_attributes(name: "Alice Smith"))
+    user2 = User.create!(user_attributes(name: "Bob Smith", email: "bob@smith.com"))
+    user3 = User.create!(user_attributes(name: "Charles Smith", email: "charles@smith.com"))
     visit users_url
     expect(page).to have_text(user1.name)
+    expect(page).to have_text(user2.name)
+    expect(page).to have_text(user3.name)
   end
 
 end
