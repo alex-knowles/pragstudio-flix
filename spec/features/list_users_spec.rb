78fbe13fbd8ab06829635abed8730c7f1668ac1d
Alex Knowles <alexk@datalogics.com>
2017-03-15 17:41:33 -0500

    Factor out some test code into a ‘before’ method.

--- spec/features/list_users_spec.rb
+++ spec/features/list_users_spec.rb
@@ -2,14 +2,17 @@ require 'rails_helper'
 
 describe "Viewing the list of users" do
 
+  before do
+    @user1 = User.create!(user_attributes(name: "Alice Smith"))
+    @user2 = User.create!(user_attributes(name: "Bob Smith", email: "bob@smith.com"))
+    @user3 = User.create!(user_attributes(name: "Charles Smith", email: "charles@smith.com"))
+  end
+
   it "shows the users" do
-    user1 = User.create!(user_attributes(name: "Alice Smith"))
-    user2 = User.create!(user_attributes(name: "Bob Smith", email: "bob@smith.com"))
-    user3 = User.create!(user_attributes(name: "Charles Smith", email: "charles@smith.com"))
     visit users_url
-    expect(page).to have_text(user1.name)
-    expect(page).to have_text(user2.name)
-    expect(page).to have_text(user3.name)
+    expect(page).to have_text(@user1.name)
+    expect(page).to have_text(@user2.name)
+    expect(page).to have_text(@user3.name)
   end
 
 end
