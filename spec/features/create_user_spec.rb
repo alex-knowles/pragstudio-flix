a0be2d69523d9b217c878fef828cb5670c0ed22c
Alex Knowles <alexk@datalogics.com>
2017-03-16 08:36:57 -0500

    Outline spec for creating a new user.

---
+++ spec/features/create_user_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+describe "Creating a user" do
+
+  it "succeeds when valid data is submitted"
+
+  it "shows the user's details after saving"
+
+  it "fails when invalid data is submitted"
+
+end
