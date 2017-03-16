168564af841588c7a28ea71bfcd141bbe6ee9003
Alex Knowles <alexk@datalogics.com>
2017-03-15 21:27:17 -0500

    Show a user’s name and email in ‘show’ view.

---
+++ spec/features/show_user_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+describe "Showing a user" do 
+
+  it "displays its attributes" do
+    @user = User.create!(user_attributes)
+    visit user_path @user
+    expect(page).to have_text(@user.name)
+    expect(page).to have_text(@user.email)
+  end
+
+end
