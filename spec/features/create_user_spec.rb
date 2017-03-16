05a2bcf0ef7f7f460c5f78d3ee075bb5437fe2da
Alex Knowles <alexk@datalogics.com>
2017-03-16 17:08:21 -0500

    Reject a user if invalid form data is submitted.
    Create view and control for user ‘create’.
    Define custom route for ‘signup’.
    Error text is shown for invalid fields.

--- spec/features/create_user_spec.rb
+++ spec/features/create_user_spec.rb
@@ -6,6 +6,13 @@ describe "Creating a user" do
 
   it "shows the user's details after saving"
 
-  it "fails when invalid data is submitted"
+  it "fails when invalid data is submitted" do
+    visit signup_url
+    expect {
+      click_button 'Sign Up'
+    }.not_to change(User, :count)
+    expect(page).to have_text('error')
+    expect(current_path).to eq(users_path)
+  end
 
 end
