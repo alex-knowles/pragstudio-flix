e115edba7f5ff3d6cc65ba3c889aef321c7273f9
Alex Knowles <alexk@datalogics.com>
2017-03-22 08:49:41 -0500

    Outline controller spec for user authentication.
    Declare some pending code examples for which
    actions require a user to sign in.

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe UsersController do
+
+  context "when not signed in" do
+
+    it "cannot access index"
+
+    it "cannot access show"
+
+    it "cannot access edit"
+
+    it "cannot access update"
+
+    it "cannot access show"
+
+  end
+
+end
