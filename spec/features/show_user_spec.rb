a4b1810e1f65e612bac9371d28ee95ceda859eae
Alex Knowles <alexk@datalogics.com>
2017-03-22 17:55:09 -0500

    Fix spec for showing a user.
    Now requires a user to be signed in.

--- spec/features/show_user_spec.rb
+++ spec/features/show_user_spec.rb
@@ -4,6 +4,7 @@ describe "Showing a user" do
 
   it "displays its attributes" do
     @user = User.create!(user_attributes)
+    sign_in(@user)
     visit user_path @user
     expect(page).to have_text(@user.name)
     expect(page).to have_text(@user.email)
