7da49c8d3c58644cdb68a38855b79b13e33d91b3
Alex Knowles <alexk@datalogics.com>
2017-03-22 17:50:52 -0500

    Collapse code examples for deleting a user.
    Both code examples for the spec became almost
    identical once sign-in was required for ‘show’.

--- spec/features/delete_user_spec.rb
+++ spec/features/delete_user_spec.rb
@@ -10,14 +10,6 @@ describe "Deleting a user" do
     expect(User.count).to eq(0)
     expect(current_url).to eq(root_url)
     expect(page).to have_text("Account deleted successfully")
-  end
-
-  it "signs the user out if signed in" do
-    user = User.create!(user_attributes)
-    sign_in(user)
-    visit user_url(user)
-    click_link "Delete Account"
-    expect(page).to have_text("Account deleted successfully")
     expect(page).not_to have_text(user.name)
     expect(page).to have_text("Sign In")
   end
