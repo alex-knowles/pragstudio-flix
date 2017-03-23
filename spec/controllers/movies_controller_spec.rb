fda1ca1b1d8ff4e5e3668dbc112b3d4137d33adc
Alex Knowles <alexk@datalogics.com>
2017-03-23 17:15:10 -0500

    Outline code examples for restricting movie access
    For movies, only admin users can:
    
    * create
    * edit
    * destroy

---
+++ spec/controllers/movies_controller_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+describe MoviesController do
+
+  context "when not signed in" do
+
+    it "cannot access create"
+
+    it "cannot access edit"
+
+    it "cannot access destroy"
+
+  end
+
+  context "when signed in as a non-admin" do
+
+    it "cannot access create"
+
+    it "cannot access edit"
+
+    it "cannot access destroy"
+
+  end
+
+end
