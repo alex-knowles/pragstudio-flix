af13384f8b3da09fc6d3cf8d92acc537e22d6523
Alex Knowles <alexk@datalogics.com>
2017-03-22 23:51:28 -0500

    Add ‘show’ user spec for seeing certain links.
    A signed-in user should see links to ‘edit’
    and ‘delete’ their account.

--- spec/features/show_user_spec.rb
+++ spec/features/show_user_spec.rb
@@ -2,12 +2,29 @@ require 'rails_helper'
 
 describe "Showing a user" do 
 
-  it "displays its attributes" do
+  before do
     @user = User.create!(user_attributes)
     sign_in(@user)
+  end
+
+  it "displays its attributes" do
     visit user_path @user
     expect(page).to have_text(@user.name)
     expect(page).to have_text(@user.email)
+  end
+
+  context "that is currently signed in" do
+
+    it "shows an 'edit' link" do
+      visit user_path @user
+      expect(page).to have_link("Edit Account")
+    end
+
+    it "shows a 'delete' link" do
+      visit user_path @user
+      expect(page).to have_link("Delete Account")
+    end
+
   end
 
 end
