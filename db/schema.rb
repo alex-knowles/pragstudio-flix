23fbe6e55f91281c8f7d20ca77f8c2f14d977b75
Alex Knowles <alexk@datalogics.com>
2017-03-23 20:43:04 -0500

    Add an admin column to Users table.
    Boolean type.
    Default value is ‘false’.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170314140845) do
+ActiveRecord::Schema.define(version: 20170324013920) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
@@ -40,8 +40,9 @@ ActiveRecord::Schema.define(version: 20170314140845) do
     t.string   "name"
     t.string   "email"
     t.string   "password_digest"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+    t.boolean  "admin",           default: false
   end
 
 end
