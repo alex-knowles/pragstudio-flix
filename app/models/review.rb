1bcf26eed4c6fb8b25cfa83973514ed2cfae0081
Alex Knowles <alexk@datalogics.com>
2017-04-19 08:56:58 -0500

    Bi-directional relationship b/w User and Review.
    1 User to Many Reviews.

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,9 +1,9 @@
 class Review < ApplicationRecord
   belongs_to :movie
+  belongs_to :user
 
   STARS = (1..5).to_a
 
-  validates :name, presence: true
   validates :comment, length: { minimum: 4 }
   validates :stars, inclusion: { in: STARS, message: "must be between 1 and 5" }
 
