502249e352484e713b2da232a48482aa79ee4fbf
Alex Knowles <alexk@datalogics.com>
2017-04-26 09:00:30 -0500

    Define “through” association called “fans”.
    A Movie has “fans”, which is the collection of
    Users contained by the Movie’s “favorites”.

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -172,4 +172,14 @@ describe "A movie" do
     }.to change(Favorite, :count).by(-1)
   end
 
+  it "has many fans" do
+    movie = Movie.new(movie_attributes)
+    fan1 = User.new(user_attributes)
+    fan2 = User.new(user_attributes)
+    movie.favorites.new(user: fan1)
+    movie.favorites.new(user: fan2)
+    expect(movie.fans).to include(fan1)
+    expect(movie.fans).to include(fan2)
+  end
+
 end
