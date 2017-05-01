4a228ed9e103ff98d8832c20852d0d0d895dec91
Alex Knowles <alexk@datalogics.com>
2017-05-01 17:21:44 -0500

    Require sign-in to ‘create’ or ‘delete’ Favorites.
    Adds an empty ‘delete’ implementation for the
    FavoritesController.  This will be implemented
    in a later commit.

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,9 +1,13 @@
 class FavoritesController < ApplicationController
+  before_action :require_signin
 
   def create
     @movie = Movie.find(params[:movie_id])
     @favorite = @movie.favorites.create!(user: current_user)
     redirect_to(@movie, notice: "Thanks for fav'ing!")
+  end
+
+  def destroy
   end
 
 end
--- spec/controllers/favorites_controller_spec.rb
+++ spec/controllers/favorites_controller_spec.rb
@@ -4,9 +4,21 @@ describe FavoritesController do
 
   context "when not signed in" do
 
-    it "cannot access create"
+    before do
+      @movie = Movie.create!(movie_attributes)
+      @user = User.create!(user_attributes)
+    end
 
-    it "cannot access destroy"
+    it "cannot access create" do
+      post :create, params: { movie_id: @movie.id, user_id: @user.id }
+      expect(response).to redirect_to(signin_url)
+    end
+
+    it "cannot access destroy" do
+      favorite = @movie.favorites.create!(user: @user)
+      delete :destroy, params: { movie_id: @movie.id, id: favorite.id }
+      expect(response).to redirect_to(signin_url)
+    end
 
   end
 
