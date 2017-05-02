b7775db5bfbc5bb2068cc0f948dabfdd9bc21d82
Alex Knowles <alexk@datalogics.com>
2017-05-02 17:58:33 -0500

    Factor out some common code in FavoritesController.
    Common logic to set the value of @movie.

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,17 +1,22 @@
 class FavoritesController < ApplicationController
   before_action :require_signin
+  before_action :set_movie
 
   def create
-    @movie = Movie.find(params[:movie_id])
     @favorite = @movie.favorites.create!(user: current_user)
     redirect_to(@movie, notice: "Thanks for fav'ing!")
   end
 
   def destroy
     @favorite = Favorite.find(params[:id])
-    @movie = Movie.find(params[:movie_id])
     @favorite.destroy!
     redirect_to(@movie, notice: "Sorry!")
+  end
+
+private
+
+  def set_movie
+    @movie = Movie.find(params[:movie_id])
   end
 
 end
