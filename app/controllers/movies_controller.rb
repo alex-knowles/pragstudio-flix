82d648c295ad0f1ac362e316daff4d2f764f50a9
Alex Knowles <alexk@datalogics.com>
2017-05-09 17:16:22 -0500

    Allow Genre form data for Movies.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -51,7 +51,7 @@ private
 
   def movie_params
     params.require(:movie).permit(:title, :description, :rating, :released_on,
-     :total_gross, :cast, :director, :duration, :image_file_name)
+     :total_gross, :cast, :director, :duration, :image_file_name, genre_ids: [])
   end
 
 end
