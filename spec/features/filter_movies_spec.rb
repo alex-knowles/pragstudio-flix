35dffb668acfbaac243b1342ae2dbb9e6660b4bd
Alex Knowles <alexk@datalogics.com>
2017-05-18 08:06:44 -0500

    Add filter links for ‘upcoming’ and ‘recent’.

--- spec/features/filter_movies_spec.rb
+++ spec/features/filter_movies_spec.rb
@@ -16,8 +16,20 @@ describe "Filtering movies" do
     expect(page).to have_text(movie.title)
   end
 
-  it "shows only 'upcoming'"
+  it "shows only 'upcoming'" do
+    movie = Movie.create!(movie_attributes(released_on: Date.tomorrow))
+    visit(root_path)
+    click_link("Upcoming")
+    expect(page).to have_text(movie.title)
+  end
 
-  it "shows only 'recent'"
+  it "shows only 'recent'" do
+    recent_movie = Movie.create!(movie_attributes(released_on: Date.yesterday))
+    not_recent_movie = Movie.create!(movie_attributes(title: "World War Hulk", released_on: Date.tomorrow))
+    visit(root_path)
+    click_link("Recent")
+    expect(page).to have_text(recent_movie.title)
+    expect(page).not_to have_text(not_recent_movie.title)
+  end
   
 end
