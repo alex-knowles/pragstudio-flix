cee9c2e3db6c84a848fe74b6d3684c069fe4715e
Alex Knowles <alexk@datalogics.com>
2017-05-17 22:14:45 -0500

    Prototype examples for filtering movies spec.
    Some links will be added to the sidebar to
    filter movies for certain scopes:
    
    * hits
    * flops
    * upcoming
    * recent

---
+++ spec/features/filter_movies_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe "Filtering movies" do
+
+  it "shows only 'hits'"
+
+  it "shows only 'flops'"
+
+  it "shows only 'upcoming'"
+
+  it "shows only 'recent'"
+  
+end
