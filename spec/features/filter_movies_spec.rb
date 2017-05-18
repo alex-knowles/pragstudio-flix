98964915021efa1f9b27433379b0ff613f70437d
Alex Knowles <alexk@datalogics.com>
2017-05-17 23:22:30 -0500

    Sidebar links to filter Movie “Hits” and “Flops.”

--- spec/features/filter_movies_spec.rb
+++ spec/features/filter_movies_spec.rb
@@ -2,9 +2,19 @@ require 'rails_helper'
 
 describe "Filtering movies" do
 
-  it "shows only 'hits'"
+  it "shows only 'hits'" do
+    movie = Movie.create!(movie_attributes(total_gross: 450000000))
+    visit(root_path)
+    click_link("Hits")
+    expect(page).to have_text(movie.title)
+  end
 
-  it "shows only 'flops'"
+  it "shows only 'flops'" do
+    movie = Movie.create!(movie_attributes(total_gross: 45000))
+    visit(root_path)
+    click_link("Flops")
+    expect(page).to have_text(movie.title)
+  end
 
   it "shows only 'upcoming'"
 
